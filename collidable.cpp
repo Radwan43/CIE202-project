81c3346f69fb96c44a2cd6933e9fff123d88c647
radwan <radwan@MAHMOUDRADWFD82>
2023-11-24 14:52:32 +0200

    Add project files.

---
+++ collidable.cpp
@@ -0,0 +1,13 @@
+#include "collidable.h"
+
+collidable::collidable(point r_uprleft, int r_width, int r_height, game* r_pGame):
+			drawable(r_uprleft, r_width, r_height,  r_pGame)
+{
+
+}
+
+// 1- Ball - Platform 
+// 2- collision physics
+
+// 3- Powerups, different brick types
+// 4- Polishing 
