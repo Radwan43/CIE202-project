0a90fd1a8c4ec6accf7284398c342e1d1cbf0c88
Mahmoud Radwan <almalek_43@hotmail.com>
2023-12-12 18:36:53 +0200

    Update drawable.h

--- drawable.h
+++ drawable.h
@@ -7,26 +7,36 @@ class game;     //forward declaration
 
 struct point
 {
-int x, y;
+    int x, y;
 };
 
-//Base class for any drawable object in the game including bricks, ball, toolbar icons,... etc
-//Each drawable object is bounded by a rectangular boundary
+// Enum to distinguish between drawing images and shapes
+enum DrawType {
+    DRAW_IMAGE,
+    DRAW_SHAPE
+};
+
+// Base class for any drawable object in the game including bricks, ball, toolbar icons,... etc
+// Each drawable object is bounded by a rectangular boundary
 class drawable
 {
 protected:
-	string imageName;	//path to its image
-    point uprLft;       //upper left corner of the drawable object
-    int width, height;  
+    DrawType drawType;  // Enum to indicate whether to draw an image or a shape
+    string imageName;   // Path to its image
+    string shapeType;   // Type of shape
+    point uprLft;       // Upper left corner of the drawable object
+    int width, height;
 
 protected:
-    game* pGame;        //pointer to game object
+    game* pGame;        // Pointer to game object
+
 public:
+    // Constructor for drawing images
     drawable(point r_uprleft, int r_width, int r_height, game* r_pGame);
-    virtual void draw() const;//for a drwable object to draw itself on the screen
-	void setImageName(string path);
+
+    // Constructor for drawing shapes
+    drawable(point r_uprleft, int r_width, int r_height, game* r_pGame, const string& shapeType);
+
+    virtual void draw() const;  // For a drawable object to draw itself on the screen
+    void setImageName(const string& path);
 };
-
-
-
-
