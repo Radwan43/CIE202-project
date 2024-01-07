ad7bcd94017f3dbddab11ededc5bd440cda25e3d
AhmedsElysium <ahmadyassermo@gmail.com>
2024-01-07 23:21:00 +0200

    MovePower

--- PowerUp.cpp
+++ PowerUp.cpp
@@ -17,15 +17,22 @@ void Power::movePower() {
     int row, column;
     pWin->SetBrush(config.bkGrndColor);
     pWin->SetPen(config.bkGrndColor, 1);
-    pWin->DrawRectangle(uprLft.x , uprLft.y, uprLft.x + config.brickHeight, uprLft.y + config.brickHeight);
+    pWin->DrawRectangle(uprLft.x, uprLft.y, uprLft.x + config.brickHeight, uprLft.y + config.brickHeight);
 
-    //if (pGame->getGrid()->getMatrix()[(clicked.y - uprLft.y) / config.brickHeight - 1][clicked.x / config.brickWidth] != NULL) {
-    //    delete pGame->getGrid()->getMatrix()[(clicked.y - uprLft.y) / config.brickHeight - 1][clicked.x / config.brickWidth];
-    //};
+
     row = (uprLft.y - config.toolBarHeight) / config.brickHeight - 1;
     column = uprLft.x / config.brickWidth;
+    if (uprLft.y > config.toolBarHeight && uprLft.y < config.gridHeight + config.toolBarHeight) {
 
-    if (pGrid->getMatrix()[row][column])
-        pGrid->getMatrix()[row][column]->draw();
+        if (pGrid->getMatrix()[row][column])
+            pGrid->getMatrix()[row][column]->draw();
+        if (row < pGrid->getrows()) {
+
+            if (pGrid->getMatrix()[row + 1][column])
+                pGrid->getMatrix()[row + 1][column]->draw();
+        };
+    };
+    uprLft.y += 5;
+    this->draw();
 
 }
